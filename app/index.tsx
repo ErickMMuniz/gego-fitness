b0718e312cdc3f4417d4c36175939d842708c88a
Erick Muñiz Morales <emm98p2@gmail.com>
2025-10-22 19:01:26 -0600

    simple index

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
-import { Text, View } from "react-native";
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Stack } from 'expo-router';
 
-export default function Index() {
+const IndexScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <Stack.Screen options={{ title: 'Gego Fitness by Erick' }} />
+      <SafeAreaView style={{ flex: 1}}>
+      <Text className='center'> Content is in safe area.</Text>
+    </SafeAreaView>
+    </>
   );
 }
+
+export default IndexScreen
